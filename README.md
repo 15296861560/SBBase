4ec90e787b27de8251f290b4885acb69d61b8557
lgy <lgy-lgy@qq.com>
2019-08-01 10:30:28 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## 资料
 https://spring.io/guides
 https://spring.io/guides/gs/serving-web-content/
-http://localhost:8887/hello?name=lgy
+http://localhost:8887/
 https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys
 ##工具
 https://git-scm.com/download
--- src/main/java/com/springboottest/demo/controller/IndexController.java
+++ src/main/java/com/springboottest/demo/controller/IndexController.java
@@ -4,15 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/index")
+    public String index(){
+        return "index";
 
     }
 }
