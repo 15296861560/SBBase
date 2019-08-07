7a11e68d53d80494f0e6e2984befb54db15db692
lgy <lgy-lgy@qq.com>
2019-08-07 09:54:00 +0800

    extract github config from code to properties

--- src/main/java/com/springboottest/demo/controller/AuthorizeController.java
+++ src/main/java/com/springboottest/demo/controller/AuthorizeController.java
@@ -4,22 +4,30 @@ import com.springboottest.demo.dto.AccessTokenDTO;
 import com.springboottest.demo.dto.GithubUser;
 import com.springboottest.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("784cb00fadf73dfb5f43");
-        accessTokenDTO.setClient_secret("1d533b285df852c24126fdb1e97b4d33b4874b48");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
