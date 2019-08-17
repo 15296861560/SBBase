102069f7942678f29b8aed2f11457b6eafd8fd27
lgy <lgy-lgy@qq.com>
2019-08-17 10:21:09 +0800

    don't refresh login

--- src/main/resources/static/js/myjs.js
+++ src/main/resources/static/js/myjs.js
@@ -14,7 +14,17 @@ function post() {
             if (response.code == 200) {
                 $("#comment_section").hidden;
             }else {
-                alert(response.message);
+                if(response.code==2003){//登陆错误则弹窗提示是否登陆
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted){//跳转到登陆地址
+                        window.open("https://github.com/login/oauth/authorize?client_id=784cb00fadf73dfb5f43&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable",true);
+                    }
+
+                }else {
+                    alert(response.message);
+
+                }
             }
             console.log(response);
 
